fc6f9eaee2b5296c4dbfba3dd8f26c1dc844962f
Gilvan Araujo <gilvanjunior@protonmail.com>
2022-10-27 06:29:18 -0300

    finish the bookings file and the project itself

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,16 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(date, origin, destination, user_id) do
+    uuid = UUID.uuid4()
+
+    {:ok,
+     %__MODULE__{
+       complete_date: date,
+       local_origin: origin,
+       local_destination: destination,
+       user_id: user_id,
+       id: uuid
+     }}
   end
 end
